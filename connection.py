57f644e9942540fc036b9d95df14152a935e9fad
Eddie Shin <eshinhw@gmail.com>
2023-07-10 17:14:47 -0400

    established connection to TWS

--- connection.py
+++ connection.py
@@ -5,4 +5,10 @@ from ibapi.wrapper import EWrapper
 class TradingApp(
     EClient, EWrapper
 ):  # all methods available in EClient and EWrapper will be available inside TradingApp class
-    pass
+    def __init__(self):
+        EClient.__init__(self, wrapper=self)
+
+
+app = TradingApp()
+app.connect("127.0.0.1", 7497, 0)
+app.run()
